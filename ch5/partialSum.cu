d2cb130b83d35d4abc73ecec77e982945da32120
gajagajago <gajagajago@naver.com>
2022-09-13 10:02:13 +0900

    partial sum opt

--- ch5/partialSum.cu
+++ ch5/partialSum.cu
@@ -2,16 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 64 // Must be a multiple of 2
-#define BLOCK_SIZE 32
+#define SIZE 101
+#define BLOCK_SIZE 4
 
+// 1d array partial sum 
+// 1 thread per 1 element
 __global__
 void partialSumKernel(float* d_X)
 {
     __shared__ float partialSum[BLOCK_SIZE];
+
+    int h_Idx = blockDim.x * blockIdx.x + threadIdx.x;
     unsigned int tx = threadIdx.x;
 
-    partialSum[tx] = d_X[blockDim.x * blockIdx.x + threadIdx.x];
+    // Boundary condition
+    if (h_Idx < SIZE) {
+        partialSum[tx] = d_X[h_Idx];
+    } else {
+        partialSum[tx] = 0.0;
+    }
 
     for (unsigned int stride = blockDim.x/2; stride >= 1; stride = stride >> 1) {
         __syncthreads();
@@ -21,7 +30,41 @@ void partialSumKernel(float* d_X)
     }
 
     if (tx == 0) {
-        d_X[blockDim.x * blockIdx.x + threadIdx.x] = partialSum[tx];
+        d_X[h_Idx] = partialSum[tx];
+    }
+}
+
+// 1d array partial sum optimized
+// 1 thread per 2 elements
+// Execution configuration parameters: dimBlock should be BLOCKSIZE / 2
+__global__
+void partialSumOptKernel(float* d_X)
+{
+    __shared__ float partialSum[BLOCK_SIZE];
+
+    int h_Idx = 2 * blockDim.x * blockIdx.x + threadIdx.x;
+    unsigned int tx = threadIdx.x;
+    unsigned int stride = blockDim.x;
+
+    // Boundary condition
+    if (h_Idx < SIZE) {
+        partialSum[tx] = d_X[h_Idx];
+        partialSum[tx + stride] = d_X[h_Idx + stride];
+    } else {
+        partialSum[tx] = 0.0;
+    }
+
+    for (; stride >= 1; stride = stride >> 1) {
+        __syncthreads();
+
+        // For the first iteration, no control divergence
+        if (tx < stride) {
+            partialSum[tx] += partialSum[tx + stride];
+        }
+    }
+
+    if (tx == 0) {
+        d_X[h_Idx] = partialSum[tx];
     }
 }
 
@@ -36,9 +79,13 @@ float partialSum(float* h_X)
 
     cudaMemcpy(d_X, h_X, size, cudaMemcpyHostToDevice);
 
-    dim3 dimGrid(SIZE/BLOCK_SIZE, 1, 1);
-    dim3 dimBlock(BLOCK_SIZE, 1, 1);
-    partialSumKernel<<<dimGrid, dimBlock>>>(d_X);
+    // dim3 dimGrid(ceil((float)SIZE/BLOCK_SIZE), 1, 1);
+    // dim3 dimBlock(BLOCK_SIZE, 1, 1);
+    // partialSumKernel<<<dimGrid, dimBlock>>>(d_X);
+
+    dim3 dimGrid(ceil((float)SIZE/BLOCK_SIZE), 1, 1);
+    dim3 dimBlock(BLOCK_SIZE/2, 1, 1); 
+    partialSumOptKernel<<<dimGrid, dimBlock>>>(d_X);
 
     // Copy only d_X[0]
     cudaMemcpy(h_X, d_X, size, cudaMemcpyDeviceToHost);
